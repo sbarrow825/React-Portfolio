4ffee509a6d5a387a2ad4e26c661ed98c4aed518
sbarrow825 <66639249+sbarrow825@users.noreply.github.com>
2020-08-25 21:59:04 -0700

    added App.js with currently empty components

---
+++ React-Portfolio/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+import AboutMe from "./components/AboutMe"
+import Assignments from "./components/Assignments"
+import Contact from "./components/Contact"
+import Footer from "./components/Footer"
+import Jumbotron from "./components/Jumbotron"
+import Navbar from "./components/Navbar"
+import Projects from "./components/Projects"
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Jumbotron />
+      <AboutMe />
+      <Projects />
+      <Assignments />
+      <Contact />
+      <Footer />      
+    </div>
+  )
+}
+
+export default App;
