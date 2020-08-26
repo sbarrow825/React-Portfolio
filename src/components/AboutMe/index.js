891ce83b7129d6359e0b30609d116ee3d8c4094a
sbarrow825 <66639249+sbarrow825@users.noreply.github.com>
2020-08-25 23:50:47 -0700

    finished AboutMe component

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -1,16 +1,24 @@
 import React from 'react';
+import "./style.css"
+import Row from "../../components/Row";
+import Col from "../../components/Col";
+import Container from "../../components/Container";
 
 function AboutMe() {
     return (
-    <div>
-        <nav className="navbar navbar-dark bg-dark justify-content-start">
-            <a className="navbar-brand" href="#aboutMe">About Me</a>
-            <a className="navbar-brand" href="#projects">Projects</a>
-            <a className="navbar-brand" href="#assignments">Assignments</a> 
-            <a className="navbar-brand" href="#contact">Contact</a> 
-        </nav>
-    </div>
+        <Container>
+            <Row>
+                <Col classes="col-md-4">
+                    <img className="ghProfile" src={"https://github.com/sbarrow825.png"} />
+                </Col>
+                <Col classes="col-md-8">
+                    <h3>About Me</h3>
+                    <p>Throughout my years at UC Berkeley, I've became greatly interested in computer science and am now looking to start my career in the technology field.</p>
+                    <p>My computer science classes at Berkeley and UC Berkeley Extension's full stack coding bootcamp program have equipped me with the neccessary skills to succeed in the tech world.</p>
+                </Col>
+            </Row>
+        </Container>
     )
-} 
+}
 
 export default AboutMe;
