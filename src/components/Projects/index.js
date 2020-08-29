4a8ff59cb448f4fc615c3708c4997a0bbf94b18b
sbarrow825 <66639249+sbarrow825@users.noreply.github.com>
2020-08-28 19:34:14 -0700

    added layout for Projects component

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -3,13 +3,27 @@ import Row from "../../components/Row";
 import Col from "../../components/Col";
 import Container from "../../components/Container";
 import Title from "../../components/Title"
+import WIP from "./assets/WIP-Logo.jpg"
+import WIPGif from "./assets/WIPGif.gif"
+import QUAN from "./assets/QUAN-Logo.png"
+import "./style.css"
 
 function Projects() {
     return (
-        <div>
+        <Container>
             <Title title={"Projects"} />
-        </div>
-        
+            <br></br>
+            <Row>
+                <Col classes="col-md-6">
+                    <h1 className="projectTitle">WIP</h1>
+                    <img src={WIP} className="WIP-Logo" alt="WIP Logo" />
+                </Col>
+                <Col classes="col-md-6">
+                    <h4>WIP is a progress tracking app that allows the user to set goals and record their progress towards them</h4>
+                    <img src={WIPGif} className="WIPGif" alt="WIP gif" />
+                </Col>
+            </Row>
+        </Container>
     )
 }
 
