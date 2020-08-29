a0ab1ea1b4c3cd584a68e1c060b983951634a992
sbarrow825 <66639249+sbarrow825@users.noreply.github.com>
2020-08-28 21:17:25 -0700

    Updates

---
+++ precache-manifest.a3829f5ed52dc94dc2882db5634f4b92.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5561391a5f78906423a83d5aed538928",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "5d0216b91bb6b29992bd",
+    "url": "/React-Portfolio/static/css/main.d77a8002.chunk.css"
+  },
+  {
+    "revision": "ee54f3c639b632674682",
+    "url": "/React-Portfolio/static/js/2.b33509bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Portfolio/static/js/2.b33509bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d0216b91bb6b29992bd",
+    "url": "/React-Portfolio/static/js/main.8e4257d1.chunk.js"
+  },
+  {
+    "revision": "b63d20c95b4097aaed6d",
+    "url": "/React-Portfolio/static/js/runtime-main.67dc4218.js"
+  },
+  {
+    "revision": "91c48f7c57221d18f9d6cf8c0ee61bff",
+    "url": "/React-Portfolio/static/media/PropertyMover.91c48f7c.png"
+  },
+  {
+    "revision": "d0e8c647d59a36f9a9ad64f53492e5e3",
+    "url": "/React-Portfolio/static/media/PropertyMover.d0e8c647.gif"
+  },
+  {
+    "revision": "4d6575f4d5d30b088adca4114f98151f",
+    "url": "/React-Portfolio/static/media/QUAN-Logo.4d6575f4.png"
+  },
+  {
+    "revision": "4468be9dbb4495a49f2f43f5fcb956d9",
+    "url": "/React-Portfolio/static/media/QUANGif.4468be9d.gif"
+  },
+  {
+    "revision": "4cf0dcb807b05d47ffbd2aaf8f17a954",
+    "url": "/React-Portfolio/static/media/WIP-Logo.4cf0dcb8.jpg"
+  },
+  {
+    "revision": "defa0859a437d71735af1c87a8cd1ae9",
+    "url": "/React-Portfolio/static/media/WIPGif.defa0859.gif"
+  },
+  {
+    "revision": "07f888764b88acc3bf30d961ea39a0a9",
+    "url": "/React-Portfolio/static/media/employeeTracker.07f88876.gif"
+  },
+  {
+    "revision": "ffa205e01880cdd22db5ae805c3f003d",
+    "url": "/React-Portfolio/static/media/googleBooksReact.ffa205e0.gif"
+  },
+  {
+    "revision": "cc393511ba03da0588f97bbd411170e1",
+    "url": "/React-Portfolio/static/media/workDayScheduler.cc393511.gif"
+  },
+  {
+    "revision": "4244e5b211af04383d54268746ab714b",
+    "url": "/React-Portfolio/static/media/workoutTracker.4244e5b2.gif"
+  }
+]);
