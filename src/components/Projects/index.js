432b8c8bc9c8f667fd662edc6520bc328fb30e12
sbarrow825 <66639249+sbarrow825@users.noreply.github.com>
2020-08-28 19:50:48 -0700

    completed Projects component

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -6,6 +6,9 @@ import Title from "../../components/Title"
 import WIP from "./assets/WIP-Logo.jpg"
 import WIPGif from "./assets/WIPGif.gif"
 import QUAN from "./assets/QUAN-Logo.png"
+import QUANGif from "./assets/QUANGif.gif"
+import PropertyMover from "./assets/PropertyMover.png"
+import PropertyMoverGif from "./assets/PropertyMover.gif"
 import "./style.css"
 
 function Projects() {
@@ -23,7 +26,33 @@ function Projects() {
                     <img src={WIPGif} className="WIPGif" alt="WIP gif" />
                 </Col>
             </Row>
+
+            <br></br>
+            <Row>
+                <Col classes="col-md-6">
+                    <h1 className="projectTitle">QUAN</h1>
+                    <img src={QUAN} className="QUAN-Logo" alt="QUAN Logo" />
+                </Col>
+                <Col classes="col-md-6">
+                    <h4>QUAN is an educational application that teaches the user about the stock market. Users are able to experiment by playing around with basic investing strategies.</h4>
+                    <img src={QUANGif} className="QUANGif" alt="QUAN gif" />
+                </Col>
+            </Row>
+
+            <br></br>
+            <br></br>
+            <Row>
+                <Col classes="col-md-6">
+                    <h1 className="projectTitle">Property Mover</h1>
+                    <img src={PropertyMover} className="PropertyMover-Logo" alt="PropertyMover Logo" />
+                </Col>
+                <Col classes="col-md-6">
+                    <h4>Property Mover is a real estate app that helps the user look for housing options when moving to a new area</h4>
+                    <img src={PropertyMoverGif} className="PropertyMoverGif" alt="PropertyMover gif" />
+                </Col>
+            </Row>
         </Container>
+
     )
 }
 
